157d87ceb27f6b429866c09be6ed1fa4602f3da3
Jose Toledo <jotatoledon@gmail.com>
2019-06-07 10:54:31 +0200

    chore: add assembly info file

---
+++ src/DotNetFunctional.Maybe/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="AssemblyInfo.cs" company="DotNetFunctional">
+// Copyright (c) DotNetFunctional. All rights reserved.
+//
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: CLSCompliant(false)]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
